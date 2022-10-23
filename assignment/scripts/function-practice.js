ebc162f0fa4379e27a3e160da7580c4c1a09a909
Khue Lee <khuelee55@gmail.com>
2022-10-23 14:51:00 -0500

    finised #2

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -10,15 +10,15 @@ function hello() {
 }
 // Call the function to test
 console.log('Test - should say "Hello World!"', hello());
-
+console.log(hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
 function helloName( name ) {
-  return;
+  return `Hello, ${name}!`;
 }
 // Remember to call the function to test
-
+console.log(helloName('Khue'));
 
 // 3. Function to add two numbers together & return the result
 function addNumbers( firstNumber ) {
